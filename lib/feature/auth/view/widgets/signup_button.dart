0df9fcd0b0ff252430c15afeb79018d08a8ac366
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2026-05-12 15:08:33 +0530

    connected sign up button to supabase auth backend

--- lib/feature/auth/view/widgets/signup_button.dart
+++ lib/feature/auth/view/widgets/signup_button.dart
@@ -1,35 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'package:vibe/core/theme/app_colors.dart';
 
-class SignUpButton extends StatelessWidget {
+import '../../provider/auth_provider.dart';
+
+class SignUpButton extends ConsumerWidget {
   const SignUpButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(50),
+
         gradient: LinearGradient(
           colors: [VibeColors.deepBlue, VibeColors.brighPurple],
         ),
       ),
+
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           fixedSize: const Size(295, 55),
+
           backgroundColor: Colors.transparent,
           shadowColor: Colors.transparent,
         ),
-        onPressed: () {},
+
+        onPressed: () async {
+          await ref.read(authViewModelProvider.notifier).loginWithGoogle();
+        },
+
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0),
+
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
+
             children: [
               FaIcon(FontAwesomeIcons.google, color: VibeColors.white),
+
               const SizedBox(width: 15),
+
               Text(
                 "Sign Up with Google",
+
                 style: TextStyle(fontSize: 18, color: VibeColors.text),
               ),
             ],
