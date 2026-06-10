c2715e22efdc22b1647af5a2c6f83d7958821aad
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2026-06-10 12:19:17 +0530

    replaced anon key with publishable key as per new syntax

--- lib/core/services/supabase_service.dart
+++ lib/core/services/supabase_service.dart
@@ -5,7 +5,7 @@ class SupabaseService {
   static Future<void> init() async {
     await Supabase.initialize(
       url: dotenv.env['SUPABASE_URL']!,
-      publishableKey: dotenv.env['SUPABASE_ANON_KEY']!,
+      publishableKey: dotenv.env['SUPABASE_PUBLISHABLE_KEY']!,
     );
   }
 
